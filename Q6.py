b1213ca3d3fbd0235beda1a6e3dc89cefc72d616
Keval Vasani <kevalv.tagline@gmail.com>
2024-06-13 14:45:41 +0530

    updated variable name

--- Q6.py
+++ Q6.py
@@ -1,12 +1,12 @@
 string = "PQRQRQRQ"
-substring = "QRQ"
-len1 = len(string)
-len2 = len(substring)
+substring = "PQR"
+string_len = len(string)
+substring_len = len(substring)
 j = 0
 count = 0
-while(j < len1):
+while(j < string_len):
     if(string[j] == substring[0]):
-        if(string[j:j+len2] == substring):
+        if(string[j:j+substring_len] == substring):
             count += 1
     j += 1
 print(count)
