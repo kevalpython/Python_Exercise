82a040824d92f12fe1228a02fe3814ed7dda34f9
Keval Vasani <kevalv.tagline@gmail.com>
2024-06-14 10:56:39 +0530

    q7.py file updated

--- Q7.py
+++ Q7.py
@@ -1,16 +1,34 @@
+"""
+Validate email list
+"""
+
 import re
 
+
 def validate_emails(emails):
+    """
+    This function is for validating emails
+
+    emails : return the list of multiple emails provided by user
+    """
     valid_emails = []
     for email in emails:
-        if re.match(r'^[\w.-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$', email):
+        if re.match(r"^[\w.-]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$", email):
             valid_emails.append(email)
     return valid_emails
 
-emails = ['abc@gmail.com', '123$tt*@xyz.com', 'good@bad@uk.in', 'nasa@usa12.space',
-          'no-reply@domain.in', 'ramhanuman@saketa.lok', 'ruhi.mohan@exter123.123',
-          'fake@fake123.fakercom']
 
-valid_emails = validate_emails(emails)
+EMAILS = [
+    "abc@gmail.com",
+    "123$tt*@xyz.com",
+    "good@bad@uk.in",
+    "nasa@usa12.space",
+    "no-reply@domain.in",
+    "ramhanuman@saketa.lok",
+    "ruhi.mohan@exter123.123",
+    "fake@fake123.fakercom",
+]
 
-print("Valid emails:", valid_emails)
+VALID_EMAILS = validate_emails(EMAILS)
+
+print("Valid emails:", VALID_EMAILS)
