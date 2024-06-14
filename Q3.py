91dba72603fe168c752a275416730b5fc57ac741
Keval Vasani <kevalv.tagline@gmail.com>
2024-06-14 10:29:58 +0530

    q3.py file updated

--- Q3.py
+++ Q3.py
@@ -1,15 +1,25 @@
-sentences = ["My name is Ram", "He is a good person", "You should be careful while coding", "He can do better", "The person is mysterious", "Jay Shree Ram", "It is my pen."]
-sentence_split=[]
-for i in sentences:
-    sentence_split.append(i.split())
-count_word={}
-for i in sentence_split:
+"""
+word tree from each sentence
+"""
+
+SENTENCES = [
+    "My name is Ram",
+    "He is a good person",
+    "You should be careful while coding",
+    "He can do better",
+    "The person is mysterious",
+    "Jay Shree Ram",
+    "It is my pen.",
+]
+SENTENCE_SPLIT = []
+for i in SENTENCES:
+    SENTENCE_SPLIT.append(i.split())
+COUNT_WORD = {}
+for i in SENTENCE_SPLIT:
     for word in i:
-        if word in count_word:
-            count_word[word]+=1
+        if word in COUNT_WORD:
+            COUNT_WORD[word] += 1
         else:
-            count_word[word]=1
-print(count_word)
-
-print(sentence_split)
-
+            COUNT_WORD[word] = 1
+print(COUNT_WORD)
+print(SENTENCE_SPLIT)
