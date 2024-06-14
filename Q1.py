63ee211856bfdf85d62f76c1216b7f28f835004a
Keval Vasani <kevalv.tagline@gmail.com>
2024-06-14 09:45:25 +0530

    q1.py file updated

--- Q1.py
+++ Q1.py
@@ -1,36 +1,34 @@
-numbers = [2, 4, 5, 2, 12, 44, 5, 1, 2, 3]
-print("A. Length of the list \nB. Display first 3 numbers \nC. Display sum of odd numbers \nD. Number of duplicate numbers \nE. Display list without duplicate numbers")
-print("list = ",numbers)
-choice=input("enter the above options : ")
-print(choice)
-without_duplicate=[]
-duplicate=[]
-if choice == "A":
-    print(len(numbers))
-elif choice == "B": 
-    for i in range(0,len(numbers)):
-        if i <= 2:
-            print(numbers[i])
-elif choice == "C":
-    odd_number=[]
-    for i in numbers:
-        if i%2!=0:
-            odd_number.append(i)
-    print(sum(odd_number))
-elif choice == "D":
-    for i in numbers:
-        if i not in without_duplicate:
-            without_duplicate.append(i)
-        else:
-            if i not in duplicate:
-                duplicate.append(i)
-    print(duplicate)
-elif choice == "E":
-    for i in numbers:
-        if i not in without_duplicate:
-            without_duplicate.append(i)
-    print(without_duplicate)
+"""
+List of 10 numbers and show the 5 different operations options 
+"""
+
+NUMBERS = [2, 4, 5, 2, 12, 44, 5, 1, 2, 3]
+print(
+    "A. Length of the list\nB. Display first 3 numbers\nC. Display sum of odd numbers\nD. Number of duplicate numbers\nE. Display list without duplicate numbers"
+)
+print("list =", NUMBERS)
+CHOICE = input("Enter the above options: ").upper()
+print(CHOICE)
+WITHOUT_DUPLICATE = []
+DUPLICATE = []
+if CHOICE == "A":
+    print(len(NUMBERS))
+elif CHOICE == "B":
+    for index, number in enumerate(NUMBERS):
+        if index < 3:
+            print(number)
+elif CHOICE == "C":
+    odd_sum = sum(number for number in NUMBERS if number % 2 != 0)
+    print(odd_sum)
+elif CHOICE == "D":
+    for number in NUMBERS:
+        if NUMBERS.count(number) > 1 and number not in DUPLICATE:
+            DUPLICATE.append(number)
+    print(DUPLICATE)
+elif CHOICE == "E":
+    for number in NUMBERS:
+        if number not in WITHOUT_DUPLICATE:
+            WITHOUT_DUPLICATE.append(number)
+    print(WITHOUT_DUPLICATE)
 else:
-    print('"'+choice+'"'+" your enter choice is not there.")
-
-
+    print(f'"{CHOICE}" your entered choice is not there.')
