4780ea06c24149b63e535179b54b21311ed16ab8
Keval Vasani <kevalv.tagline@gmail.com>
2024-06-14 11:03:37 +0530

    q8.py file updated

--- Q8.py
+++ Q8.py
@@ -1,13 +1,28 @@
-def sorting(array,size):
+"""
+Sorting List
+"""
+
+
+def sorting(array, size):
+    """
+    this function sort the given array 
+    
+    args : arrays = give the list of number which we have to sort 
+           size = give the array length
+    
+    this function return the sorted array of given array
+    """
     for i in range(size):
-        index=i
-        for j in range(i+1,size):
+        index = i
+        for j in range(i + 1, size):
             if array[j] < array[index]:
-                index=j
-            (array[i],array[index])=(array[index],array[i])
+                index = j
+            (array[i], array[index]) = (array[index], array[i])
     print(array)
-data=[1,4,2,7,8,6,3]
-size=len(data)
-print(data)
+
+
+DATA = [1, 4, 2, 7, 8, 6, 3]
+SIZE = len(DATA)
+print(DATA)
 print("With selection sort.")
-sorting(data,size)
+sorting(DATA, SIZE)
