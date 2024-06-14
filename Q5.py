f8c779b5ef69485ad34fa01af3e9bad1c2d1213a
Keval Vasani <kevalv.tagline@gmail.com>
2024-06-14 10:48:59 +0530

    q5.py file updated

--- Q5.py
+++ Q5.py
@@ -1,11 +1,18 @@
-numbers = [9, 4, 8, 10, 2, 4, 8, 3, 14, 4, 8]
-n = 12
-res = []
-for i in range(len(numbers)):
-    index=i
-    for j in range(i+1,len(numbers)):
-        total=numbers[index] + numbers[j]
-        if total==n:
-            if (numbers[index],numbers[j]) not in res and (numbers[j],numbers[index]) not in res:
-                res.append((numbers[index],numbers[j]))
-print(res)
+"""
+Find pairs in list whose sum equal to given number
+"""
+
+NUMBERS = [9, 4, 8, 10, 2, 4, 8, 3, 14, 4, 8]
+NUM = 12
+RESULT = []
+for i in range(len(NUMBERS)):
+    index = i
+    for j in range(i + 1, len(NUMBERS)):
+        total = NUMBERS[index] + NUMBERS[j]
+        if total == NUM:
+            if (NUMBERS[index], NUMBERS[j]) not in RESULT and (
+                NUMBERS[j],
+                NUMBERS[index],
+            ) not in RESULT:
+                RESULT.append((NUMBERS[index], NUMBERS[j]))
+print(RESULT)
