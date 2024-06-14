07929563dded72670b190d9b10f1ee4389bf23d6
Keval Vasani <kevalv.tagline@gmail.com>
2024-06-14 10:44:45 +0530

    q4.py file updated

--- Q4.py
+++ Q4.py
@@ -1,18 +1,31 @@
-def fibonacci(n,memo={}):
-    print(memo)
-    if n in memo:
+"""
+Fibonacci Series
+"""
+
+def fibonacci(n, memo=None):
+    """
+    This function generates the Fibonacci series up to the Nth term.
+
+    Args:
+    n (int): The end term of the Fibonacci series.
         
+    Returns:
+    int: The Fibonacci series up to the Nth term.
+    """
+    if memo is None:
+        memo = {}
+
+    if n in memo:
         return memo[n]
     if n <= 1:
         return n
-    else:
-        memo[n] = fibonacci(n-1,memo) + fibonacci(n-2,memo)
-        return memo[n]
+    memo[n] = fibonacci(n-1, memo) + fibonacci(n-2, memo)
+    return memo[n]
 
 N = 8
-result = fibonacci(N)
-print("Output:", result)
-fib_60 = fibonacci(60)
-fib_90 = fibonacci(90)
-print("60th Fibonacci term:", fib_60)
-print("90th Fibonacci term:", fib_90)
+RESULT = fibonacci(N)
+print("Output:", RESULT)
+FIB_60 = fibonacci(60)
+FIB_90 = fibonacci(90)
+print("60th Fibonacci term:", FIB_60)
+print("90th Fibonacci term:", FIB_90)
