61dd0bb145757e61543c89e5fa3bc65306dc1050
Keval Vasani <kevalv.tagline@gmail.com>
2024-06-14 10:17:52 +0530

    q11.py file updated

--- Q11.py
+++ Q11.py
@@ -1,29 +1,50 @@
-wrong_AP = [2, 5, 8, 11, 15, 17]
-wrong_GP = [2, 4, 8, 16, 15, 64 ]
+"""
+Arithmetic and Geometric progression
+"""
+
+WRONG_AP = [2, 5, 8, 11, 15, 17]
+WRONG_GP = [2, 4, 8, 16, 15, 64]
+
 
 def arithmetic_progression(numbers):
+    """
+    This Function show Arithmetic progression
+
+    number : its return the given list of numbers by user
+
+    return : this function return the arithmetic progress of given list
+    """
     common_difference = numbers[1] - numbers[0]
     for i in range(1, len(numbers) - 1):
         if numbers[i + 1] - numbers[i] != common_difference:
-            wrong_index = i+1
+            wrong_index = i + 1
             break
     correct_value = numbers[i] + common_difference
     numbers[wrong_index] = correct_value
     return numbers
 
+
 def geometric_progression(numbers):
+    """
+    This Function show Geometric progression
+
+    number : its return the given list of numbers by user
+
+    return : this function return the geometric progress of given list
+    """
     common_ratio = numbers[1] / numbers[0]
     for i in range(1, len(numbers) - 1):
         if numbers[i + 1] / numbers[i] != common_ratio:
-            wrong_index = i+1
+            wrong_index = i + 1
             break
     print(wrong_index)
     correct_value = numbers[i] * common_ratio
     numbers[wrong_index] = correct_value
     return numbers
 
-correct_AP = arithmetic_progression(wrong_AP)
-correct_GP = geometric_progression(wrong_GP)
 
-print("Correct A.P.:", correct_AP)
-print("Correct G.P.:", correct_GP)
+CORRECT_AP = arithmetic_progression(WRONG_AP)
+CORRECT_GP = geometric_progression(WRONG_GP)
+
+print("Correct A.P.:", CORRECT_AP)
+print("Correct G.P.:", CORRECT_GP)
